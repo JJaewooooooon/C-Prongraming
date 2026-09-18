25d946f63ba40b8f6b8afded2de577b588e38f1e
재원 <mopom1878@gmail.com>
2026-09-18 11:19:16 +0900

    3week

---
+++ 3w실습4.cpp
@@ -0,0 +1,13 @@
+#include <cstdio>
+
+int main(void)
+{
+	int num = 0;
+
+	while(num<5)
+	{	
+		printf("Hello World! %d \n", num);
+		num++;
+	}
+	return 0;
+}
---
+++ 3w실습6.cpp
@@ -0,0 +1,14 @@
+#include <cstdio>
+
+int main(void)
+{
+	int total = 0, num = 0;
+	do
+	{
+		printf("정수 입력(0 to quit): ");
+		(void)scanf("%d", &num);
+		total += num;
+	} while (num != 0);
+	printf("합계: %d \n", total);
+	return 0;
+}
